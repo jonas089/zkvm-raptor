9a768d8fae49db61b3493ce5aceba411ddcca7d9
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-11-22 22:34:05 +0100

    serialize outputs

--- miden-raptor/proving-system/src/lib.rs
+++ miden-raptor/proving-system/src/lib.rs
@@ -21,10 +21,10 @@ impl Default for Fibonacci{
     }
 }
 impl Fibonacci{
-    fn prove(&self){
+    fn prove(&self) -> (StackOutputs, ExecutionProof, miden::Program){
         let program: miden::Program = Assembler::default().compile(&self.source).unwrap();
         let host: DefaultHost<miden::MemAdviceProvider> = DefaultHost::default();
-        let stack_inputs: StackInputs = StackInputs::try_from_values([0,1]).unwrap();
+        let stack_inputs: StackInputs = StackInputs::try_from_values([1]).unwrap();
         let (outputs, proof) = miden::prove(
             &program,
             stack_inputs,
@@ -32,28 +32,27 @@ impl Fibonacci{
             ProvingOptions::default(),
         )
         .unwrap();
-        let stack = outputs.stack_truncated(1);
+        //let stack = outputs.stack_truncated(1);
+        (outputs, proof, program)
     }
-    fn verify(&self, proof: ExecutionProof, program: miden::Program){
-        
+    fn verify(&self, outputs: StackOutputs, proof: ExecutionProof, program: miden::Program) -> bool{
+        let is_valid = miden::verify(ProgramInfo::from(program), StackInputs::try_from_values([0, 1]).unwrap(),outputs,  proof);
+        match is_valid{
+            Ok(_) => true,
+            Err(_) => false
+        }
     }
 }
 
 #[test]
 fn test_fibonacci(){
-    // set the number of terms to compute
-    let n = 50;
-
     // instantiate the default assembler and compile the program
     let source = fibonacci_assembler(50);
     let program = Assembler::default().compile(&source).unwrap();
-
     // initialize a default host (with an empty advice provider)
     let host = DefaultHost::default();
-
     // initialize the stack with values 0 and 1
     let stack_inputs = StackInputs::try_from_values([0, 1]).unwrap();
-
     // execute the program
     let (outputs, proof) = miden::prove(
         &program,
@@ -62,16 +61,39 @@ fn test_fibonacci(){
         ProvingOptions::default(), // use default proving options
     )
     .unwrap();
-
     // fetch the stack outputs, truncating to the first element
     let stack = outputs.stack_truncated(1);
-
     // the output should be the 50th Fibonacci number
     assert_eq!(&[12586269025], stack);
-
     let is_valid = miden::verify(ProgramInfo::from(program), StackInputs::try_from_values([0, 1]).unwrap(),outputs,  proof);
     match is_valid{
         Ok(_) => println!("[OK] Execution was verified successfully!"),
         Err(_) => eprintln!("[Err] Failed to verify execution!")
     };
+}
+
+#[test]
+fn test_fibonacci_struct(){
+    let fibonacci = Fibonacci::default();
+    let proof = fibonacci.prove();
+    println!("Is valid: {:?}", fibonacci.verify(proof.0, proof.1, proof.2))
+}
+
+/*
+serialize and deserialize 
+    - Outputs
+    - ExecutionProof
+    - Program
+*/
+#[test]
+fn try_serialization(){
+    let fibonacci = Fibonacci::default();
+    let proof = fibonacci.prove();
+    let outputs = proof.0;
+    let outputs_serialized = outputs.stack();
+
+    //let stack_inputs: StackInputs = StackInputs::try_from_values([1]).unwrap();
+    let stack_outputs = StackOutputs::new(outputs_serialized.to_vec(), Vec::new()).unwrap();
+    assert_eq!(outputs, stack_outputs);
+    println!("Outputs serialized: {:?}", outputs_serialized);
 }
